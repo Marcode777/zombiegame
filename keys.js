568ec87c2494d16970fb197165d9f19d3dc4e43a
Marcode777 <marc102202@hotmail.com>
2016-01-19 10:50:12 -0500

    added key information such as consumer key, consumer secret, and access key and access secret, information to keys.js file

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<HX13afY78Qp8WMe90LCezwiwa>',
+  consumer_secret: '<rvj9vDkgQ4c8VmesRouURHGmFELQ5XKvFtLdXyzzMeldtCPB96>',
+  access_token_key: '<4784330371-flMj0vlGzB7btjplxT8nHVKRChZWhHZsfBWgWkD>',
+  access_token_secret: '<3nEBK2iDuJO6IE8l0XZvNcAILNS5dHiyVbFiwRCKscoHu
+>',
+}
+
+// not sure if the keys have to have the '' quotes and < or > carats
